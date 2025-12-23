e65c0b61d31763860ab2280e26e2c1e3200202e6
honeylemonicetea <dark.shadow2019@yahoo.com>
2025-12-23 03:55:33 +0300

    flashcards

---
+++ script.js
@@ -0,0 +1,374 @@
+let words = [
+  {
+    word: "be",
+    translation: "быть, являться",
+    img: "img/be.jpg",
+  },
+  {
+    word: "call",
+    translation: "звонить, звать",
+    img: "img/call.png",
+  },
+  {
+    word: "run",
+    translation: "бегать",
+    img: "img/run.png",
+  },
+  {
+    word: "read",
+    translation: "читать",
+    img: "img/read.avif",
+  },
+  {
+    word: "like",
+    translation: "предпочитать, нравиться",
+    img: "img/like.jpg",
+  },
+  {
+    word: "ask",
+    translation: "спросить",
+    img: "img/ask.avif",
+  },
+  {
+    word: "do",
+    translation: "делать",
+    img: "img/do.jpg",
+  },
+  {
+    word: "understand",
+    translation: "понимать",
+    img: "img/understand.jpeg",
+  },
+  {
+    word: "move",
+    translation: "двигаться",
+    img: "img/move.avif",
+  },
+  {
+    word: "try",
+    translation: "пытаться",
+    img: "img/try.avif",
+  },
+  {
+    word: "have",
+    translation: "иметь",
+    img: "img/have.webp",
+  },
+  {
+    word: "watch",
+    translation: "смотреть",
+    img: "img/watch.jpeg",
+  },
+  {
+    word: "say",
+    translation: "говорить",
+    img: "img/say.png",
+  },
+  {
+    word: "need",
+    translation: "нуждаться",
+    img: "img/need.png",
+  },
+  {
+    word: "live",
+    translation: "жить",
+    img: "img/live.png",
+  },
+  {
+    word: "follow",
+    translation: "следовать",
+    img: "img/follow.png",
+  },
+  {
+    word: "go",
+    translation: "идти",
+    img: "img/go.png",
+  },
+  {
+    word: "feel",
+    translation: "чувствовать",
+    img: "img/feel.png",
+  },
+  {
+    word: "believe",
+    translation: "верить",
+    img: "img/believe.avif",
+  },
+  {
+    word: "stop",
+    translation: "остановиться",
+    img: "img/stop.png",
+  },
+  {
+    word: "drive",
+    translation: "ехать",
+    img: "img/drive.png",
+  },
+  {
+    word: "buy",
+    translation: "купить",
+    img: "img/buy.png",
+  },
+  {
+    word: "hold",
+    translation: "держать",
+    img: "img/hold.jpg",
+  },
+  {
+    word: "create",
+    translation: "создавать",
+    img: "img/create.png",
+  },
+  {
+    word: "get",
+    translation: "получить",
+    img: "img/get.png",
+  },
+  {
+    word: "wait",
+    translation: "ждать",
+    img: "img/wait.png",
+  },
+  {
+    word: "bring",
+    translation: "приносить",
+    img: "img/bring.png",
+  },
+  {
+    word: "speak",
+    translation: "говорить",
+    img: "img/speak.jpg",
+  },
+  {
+    word: "make",
+    translation: "делать, создавать",
+    img: "img/make.png",
+  },
+  {
+    word: "put",
+    translation: "ставить",
+    img: "img/put.png",
+  },
+  {
+    word: "remember",
+    translation: "помнить",
+    img: "img/remember.png",
+  },
+  {
+    word: "know",
+    translation: "знать",
+    img: "img/know.png",
+  },
+  {
+    word: "send",
+    translation: "отправлять",
+    img: "img/send.png",
+  },
+  {
+    word: "write",
+    translation: "написать",
+    img: "img/write.jpg",
+  },
+  {
+    word: "think",
+    translation: "думать",
+    img: "img/think.png",
+  },
+  {
+    word: "keep",
+    translation: "хранить, оставлять себе",
+    img: "img/keep.png",
+  },
+  {
+    word: "walk",
+    translation: "гулять, ходить",
+    img: "img/walk.jpg",
+  },
+  {
+    word: "take",
+    translation: "взять",
+    img: "img/take.png",
+  },
+  {
+    word: "let",
+    translation: "позволять",
+    img: "img/let.jpeg",
+  },
+  {
+    word: "sit",
+    translation: "сидеть",
+    img: "img/sit.png",
+  },
+  {
+    word: "see",
+    translation: "видеть",
+    img: "img/see.png",
+  },
+  {
+    word: "begin",
+    translation: "начать",
+    img: "img/begin.png",
+  },
+  {
+    word: "stand",
+    translation: "стоять",
+    img: "img/stand.png",
+  },
+  {
+    word: "come",
+    translation: "приходить",
+    img: "img/come.png",
+  },
+  {
+    word: "fall",
+    translation: "падать",
+    img: "img/fall.png",
+  },
+  {
+    word: "lose",
+    translation: "потерять, проиграть",
+    img: "img/lose.png",
+  },
+  {
+    word: "want",
+    translation: "хотеть",
+    img: "img/want.png",
+  },
+
+  {
+    word: "help",
+    translation: "помогать",
+    img: "img/help.avif",
+  },
+  {
+    word: "win",
+    translation: "выиграть",
+    img: "img/win.png",
+  },
+  {
+    word: "look",
+    translation: "смотреть",
+    img: "img/look.jpg",
+  },
+  {
+    word: "talk",
+    translation: "говорить, разговаривать",
+    img: "img/talk.png",
+  },
+  {
+    word: "use",
+    translation: "использовать",
+    img: "img/use.png",
+  },
+  {
+    word: "meet",
+    translation: "встречать",
+    img: "img/meet.png",
+  },
+  {
+    word: "cut",
+    translation: "резать",
+    img: "img/cut.png",
+  },
+  {
+    word: "open",
+    translation: "открывать",
+    img: "img/open.png",
+  },
+  {
+    word: "find",
+    translation: "найти",
+    img: "img/find.png",
+  },
+  {
+    word: "start",
+    translation: "начать",
+    img: "img/start.avif",
+  },
+  {
+    word: "continue",
+    translation: "продолжать",
+    img: "img/continue.avif",
+  },
+  {
+    word: "give",
+    translation: "давать",
+    img: "img/give.png",
+  },
+  {
+    word: "show",
+    translation: "показывать",
+    img: "img/show.png",
+  },
+  {
+    word: "tell",
+    translation: "рассказать",
+    img: "img/tell.avif",
+  },
+  {
+    word: "grow",
+    translation: "расти",
+    img: "img/grow.png",
+  },
+  {
+    word: "hear",
+    translation: "слышать",
+    img: "img/hear.webp",
+  },
+  {
+    word: "learn",
+    translation: "учиться",
+    img: "img/learn.avif",
+  },
+  {
+    word: "work",
+    translation: "работать",
+    img: "img/work.avif",
+  },
+  {
+    word: "play",
+    translation: "играть",
+    img: "img/play.webp",
+  },
+  {
+    word: "add",
+    translation: "добавить",
+    img: "img/add.png",
+  },
+];
+
+let cards = [];
+let cardsContainer = document.querySelector(".cards-container");
+words.map((word) => {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  let cardBack = document.createElement("div");
+  cardBack.classList.add("card-back");
+  cardBack.innerHTML = `
+        <h1 class="card-word">${word.word}</h1>
+        <img src="${word.img}" alt="">
+        <h2 class="card-translation">${word.translation}</h2>`;
+  let cardFront = document.createElement("div");
+  cardFront.classList.add("card-front");
+  cardFront.innerHTML = `<h1 class="card-word">${word.word}</h1>`;
+  card.appendChild(cardBack);
+  card.appendChild(cardFront);
+  cardsContainer.appendChild(card);
+  card.addEventListener("click", toggleOpenCard);
+  cards.push(card);
+});
+
+function toggleOpenCard(event) {
+    console.log("function")
+    let targetCard
+    if(event.target.parentElement.classList.contains("card")){
+        targetCard = event.target.parentElement
+    }
+    if (event.target.parentElement.classList.contains("card-back") || event.target.parentElement.classList.contains("card-front")){
+        targetCard = event.target.parentElement.parentElement
+    }
+    if (targetCard) {
+        targetCard.classList.toggle("card_open");
+        console.log("card open")
+  }
+}
